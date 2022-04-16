ba2fc99dbe836f4e4e441ebc1d49f2f1149561d2
eM4dri <edu.mail.delivery+github@gmail.com>
2022-04-16 17:37:36 +0200

    Pars error  handling performance

--- src/parser/fill_struct_by_type.c
+++ src/parser/fill_struct_by_type.c
@@ -6,7 +6,7 @@
 /*   By: emadriga <emadriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 18:11:58 by jvacaris          #+#    #+#             */
-/*   Updated: 2022/04/16 13:49:58 by emadriga         ###   ########.fr       */
+/*   Updated: 2022/04/16 17:25:05 by emadriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ t_item	*line2alight(char **line)
 	item = malloc(sizeof(t_item));
 	item->type = ALIGHT;
 	error += try_set_atof(&item->brightness, line[1], 0.0, 1.0);
-	error += try_set_color(&item->color, line[2]);
+	if (!error)
+		error += try_set_color(&item->color, line[2]);
 	if (error)
 		ft_free((void **) &item);
 	return (item);
@@ -43,8 +44,10 @@ t_item	*line2camera(char **line)
 	item->type = CAMERA;
 	error += try_set_coords(&item->loc, line[1], MIN_PARSED_SIZE, \
 			MAX_PARSED_SIZE);
-	error += try_set_coords(&item->orient, line[2], -1.0, 1.0);
-	error += try_set_atof(&item->fov, line[3], 0.0, 180.0);
+	if (!error)
+		error += try_set_coords(&item->orient, line[2], -1.0, 1.0);
+	if (!error)
+		error += try_set_atof(&item->fov, line[3], 0.0, 180.0);
 	if (error)
 		ft_free((void **) &item);
 	return (item);
@@ -62,8 +65,10 @@ t_item	*line2light(char **line)
 	item->type = LIGHT;
 	error += try_set_coords(&item->loc, line[1], MIN_PARSED_SIZE, \
 			MAX_PARSED_SIZE);
-	error += try_set_atof(&item->brightness, line[2], 0.0, 1.0);
-	error += try_set_color(&item->color, line[3]);
+	if (!error)
+		error += try_set_atof(&item->brightness, line[2], 0.0, 1.0);
+	if (!error)
+		error += try_set_color(&item->color, line[3]);
 	if (DEFAULT_LIGHT_COLOR)
 		item->color = set_color(SET_DEFAULT_LIGHT_COLOR);
 	if (error)
--- src/parser/fill_struct_by_type_figures.c
+++ src/parser/fill_struct_by_type_figures.c
@@ -6,7 +6,7 @@
 /*   By: emadriga <emadriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 18:11:58 by jvacaris          #+#    #+#             */
-/*   Updated: 2022/04/16 13:51:16 by emadriga         ###   ########.fr       */
+/*   Updated: 2022/04/16 17:27:55 by emadriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,10 @@ t_item	*line2sphere(char **line)
 	item->type = SPHERE;
 	error += try_set_coords(&item->loc, line[1], MIN_PARSED_SIZE, \
 			MAX_PARSED_SIZE);
-	error += try_set_atof(&item->diameter, line[2], 0.0, MAX_PARSED_SIZE);
-	error += try_set_color(&item->color, line[3]);
+	if (!error)
+		error += try_set_atof(&item->diameter, line[2], 0.0, MAX_PARSED_SIZE);
+	if (!error)
+		error += try_set_color(&item->color, line[3]);
 	if (error)
 		ft_free((void **) &item);
 	return (item);
@@ -43,8 +45,10 @@ t_item	*line2plane(char **line)
 	item->type = PLANE;
 	error += try_set_coords(&item->loc, line[1], MIN_PARSED_SIZE, \
 			MAX_PARSED_SIZE);
-	error += try_set_coords(&item->orient, line[2], -1.0, 1.0);
-	error += try_set_color(&item->color, line[3]);
+	if (!error)
+		error += try_set_coords(&item->orient, line[2], -1.0, 1.0);
+	if (!error)
+		error += try_set_color(&item->color, line[3]);
 	if (error)
 		ft_free((void **) &item);
 	return (item);
@@ -62,9 +66,15 @@ t_item	*line2cylinder(char **line)
 	item->type = CYLINDER;
 	error += try_set_coords(&item->loc, line[1], MIN_PARSED_SIZE, \
 			MAX_PARSED_SIZE);
-	error += try_set_coords(&item->orient, line[2], -1.0, 1.0);
-	error += try_set_atof(&item->diameter, line[3], 0.0, MAX_PARSED_SIZE);
-	error += try_set_atof(&item->diameter, line[4], 0.0, MAX_PARSED_SIZE);
-	error += try_set_color(&item->color, line[5]);
+	if (!error)
+		error += try_set_coords(&item->orient, line[2], -1.0, 1.0);
+	if (!error)
+		error += try_set_atof(&item->diameter, line[3], 0.0, MAX_PARSED_SIZE);
+	if (!error)
+		error += try_set_atof(&item->diameter, line[4], 0.0, MAX_PARSED_SIZE);
+	if (!error)
+		error += try_set_color(&item->color, line[5]);
+	if (error)
+		ft_free((void **) &item);
 	return (item);
 }
