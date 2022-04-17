bc7c761926dddd599b712e9400baffc8b6563e56
Jorge Vacarisas bofill <jvacaris@c3r1s3.42madrid.com>
2022-04-17 20:07:58 +0200

    Fixed the effing bug with the lights

--- src/coloring/get_collision_point.c
+++ src/coloring/get_collision_point.c
@@ -6,7 +6,7 @@
 /*   By: jvacaris <jvacaris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 19:55:21 by jvacaris          #+#    #+#             */
-/*   Updated: 2022/04/16 19:49:05 by jvacaris         ###   ########.fr       */
+/*   Updated: 2022/04/17 20:05:25 by jvacaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,27 +58,37 @@ t_figure_point	get_closest_fig_point(t_vectors ray, t_item *items)
 	t_figure_point	top_point;
 	t_item			item_alight;
 	int				ctrl;
+	int				ctrl2;
 	t_item			*items2;
 
 	ctrl = 0;
+	ctrl2 = 0;
 	items2 = items;
 	item_alight = get_item_by_type(&items, ALIGHT);
 	while (items)
 	{
 		if (items->type == SPHERE && touches_sphere(ray, *(items)))
+		{
 			new_point = get_sphere_point(ray, *(items));
+			ctrl2 = 1;
+		}
 		else if (items->type == PLANE && touches_plane(ray, *(items)))
+		{
 			new_point = get_plane_point(ray, *(items));
-		if (!ctrl || getmodule(v_v_sub(new_point.loc, ray.loc)) < \
-			getmodule(v_v_sub(top_point.loc, ray.loc)))
+			ctrl2 = 1;
+		}
+		if (ctrl2 && (!ctrl || getmodule(v_v_sub(new_point.loc, ray.loc)) < \
+			getmodule(v_v_sub(top_point.loc, ray.loc))))
 		{
 			top_point = new_point;
 			ctrl = 1;
+			ctrl2 = 0;
 		}
 		if (items->next == NULL)
 			break ;
 		items = items->next;
 	}
+//	printf("Loc: %.2f %.2f %.2f    id: %d\n", top_point.loc.x, top_point.loc.y, top_point.loc.z, top_point.id);
 	calculate_reflection(&top_point, items2, item_alight);
 	return (top_point);
 }
